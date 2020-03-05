c1159e5ca3f81c4d8a24e205ad89f98efca950a9
nedjulius <juliusned@Juliuss-MBP.lan>
2020-03-05 01:52:01 -0500

    Some style changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import CurrencyInput from './components/CurrencyInput'
 import SliderInput from './components/SliderInput'
 import DisplayGraph from './components/DisplayGraph'
@@ -9,30 +9,33 @@ import useCalculate from './hooks/use_calculate'
 
 const App = () => {
 	const [setFrequency, setInit, setMonthly, setRate, loading, data] = useCalculate();
-	console.log(loading);
+
 	return (
+		<React.Fragment>
+		<div className="header-banner">
+			<h1 className="fmz-white-font">Finimize Interest Rate Calculator</h1>
+		</div>
 		<div className="App">
-			<div className="header-banner">
-				<h1 className="fmz-white-font">Finimize Interest Rate Calculator</h1>
-			</div>
-			<div className="financial-inputs">
-				<p className="input-label">How much have you saved?</p>
-				<CurrencyInput defaultValue={0} update={setInit}/>
+			<div className="inputs">
+				<div className="financial-inputs">
+					<p className="input-label">How much have you saved?</p>
+					<CurrencyInput defaultValue={1} update={setInit}/>
 
-				<p className="input-label">How much will you save each month?</p>
-				<CurrencyInput defaultValue={0} update={setMonthly}/>
+					<p className="input-label">How much will you save each month?</p>
+					<CurrencyInput defaultValue={1} update={setMonthly}/>
 
-				<p className="input-label">How much interest will you earn per year?</p>
-				<SliderInput defaultValue={4} update={setRate}/>
+					<p className="input-label">How much interest will you earn per year?</p>
+					<SliderInput defaultValue={0.25} update={setRate}/>
 
-				<FrequencyInput defaultValue={"monthly"} update={setFrequency} />
+					<p className="input-label">How often will your interest be paid?</p>
+					<FrequencyInput defaultValue={"monthly"} update={setFrequency} />
+				</div>
 			</div>
 			<div className="financial-display">
-				{/*We have included some sample data here, you will need to replace this
-				with your own. Feel free to change the data structure if you wish.*/}
-				<DisplayGraph data={data}/>
+				<DisplayGraph data={data} loading={loading}/>
 			</div>
 		</div>
+		</React.Fragment>
 	);
 }
 
