6e4fad4d731e96ca6074c7df4133fe39f0cc5859
nedjulius <juliusned@Juliuss-MBP.lan>
2020-03-05 01:21:39 -0500

    Introduced hooks to slider

--- client/src/components/SliderInput.js
+++ client/src/components/SliderInput.js
@@ -1,39 +1,29 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import './SliderInput.css'
 
-export default class SliderInput extends Component {
+const SliderInput = ({ defaultValue, update }) => {
+	const [value, setValue] = useState(defaultValue);
+	useEffect(() => {
+		update(value);
+	}, [value]);
 
-	constructor(props) {
-		super(props)
-
-		this.state = {
-			value: props.defaultValue
-		}
-	}
-
-	handleChange(e) {
-		const value = e.target.value
-		this.setState({value})
-	}
-
-	render() {
-		const { value } = this.state
-
-		return (
-			<div className="fmz-slider">
-				<p>{value}%</p>
-				<input type="range"
-					value={value}
-					min={0}
-					max={10}
-					step={0.25}
-					onChange={this.handleChange.bind(this)}/>
-			</div>
-		)
-	}
+	const handleChange = (e) => setValue(e.target.value);
+	return (
+		<div className="fmz-slider">
+			<p>{value}%</p>
+			<input type="range"
+				value={value}
+				min={0}
+				max={10}
+				step={0.25}
+				onChange={handleChange}/>
+		</div>
+	)
 }
 
 SliderInput.propTypes = {
 	defaultValue: PropTypes.number
 }
+
+export default SliderInput;
