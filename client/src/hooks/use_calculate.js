20a548c03ae2eae0bd298f78d977f0497defed76
nedjulius <juliusned@Juliuss-MBP.lan>
2020-03-05 01:09:30 -0500

    Created data hook

---
+++ client/src/hooks/use_calculate.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react'
+import { getCalculations } from '../requests'
+
+const useCalculate = () => {
+  const [frequency, setFrequency] = useState('annually');
+  const [init, setInit] = useState(0);
+  const [monthly, setMonthly] = useState(0);
+  const [rate, setRate] = useState(2.3);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([{ month: 1, amount: 0 }]);
+
+  useEffect(() => {
+    setLoading(true);
+    requestData();
+  }, [frequency, init, monthly, rate])
+
+  const requestData = async () => {
+    try {
+      const res = await getCalculations({ frequency, init, monthly, rate });
+      console.log(res.data)
+      setData(res.data);
+    } catch (e) {
+      console.log("An error occured...");
+      console.error(e);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return [
+    setFrequency,
+    setInit,
+    setMonthly,
+    setRate,
+    loading,
+    data
+  ]
+}
+
+export default useCalculate;
