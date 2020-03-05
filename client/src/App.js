3d25fd5037cf659906287208497886b6cb9822a4
nedjulius <juliusned@Juliuss-MBP.lan>
2020-03-05 01:13:06 -0500

    Updated components with useHooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,38 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import CurrencyInput from './components/CurrencyInput'
 import SliderInput from './components/SliderInput'
 import DisplayGraph from './components/DisplayGraph'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="header-banner">
-          <h1 className="fmz-white-font">Finimize Interest Rate Calculator</h1>
-        </div>
-				<div className="financial-inputs">
-					<p className="input-label">How much have you saved?</p>
-					<CurrencyInput defaultValue={0}/>
+import useCalculate from './hooks/use_calculate'
 
-					<p className="input-label">How much will you save each month?</p>
-					<CurrencyInput defaultValue={0}/>
+import { getCalculations } from './requests'
 
-					<p className="input-label">How much interest will you earn per year?</p>
-					<SliderInput defaultValue={4}/>
-				</div>
-				<div className="financial-display">
-					{/*We have included some sample data here, you will need to replace this
-					with your own. Feel free to change the data structure if you wish.*/}
-					<DisplayGraph data={[
-						{
-							month: 1,
-							amount:500
-						},
-						{
-							month: 2,
-							amount:700
-						},
-						{
-							month: 3,
-							amount:1000
-						},
-						{
-							month: 4,
-							amount:1500
-						}
-					]}/>
-				</div>
-      </div>
-    );
-  }
+const App = () => {
+	const [setFrequency, setInit, setMonthly, setRate, loading, data] = useCalculate();
+	console.log(loading);
+	return (
+		<div className="App">
+			<div className="header-banner">
+				<h1 className="fmz-white-font">Finimize Interest Rate Calculator</h1>
+			</div>
+			<div className="financial-inputs">
+				<p className="input-label">How much have you saved?</p>
+				<CurrencyInput defaultValue={0} update={setInit}/>
+
+				<p className="input-label">How much will you save each month?</p>
+				<CurrencyInput defaultValue={0} update={setMonthly}/>
+
+				<p className="input-label">How much interest will you earn per year?</p>
+				<SliderInput defaultValue={4} update={setRate}/>
+			</div>
+			<div className="financial-display">
+				{/*We have included some sample data here, you will need to replace this
+				with your own. Feel free to change the data structure if you wish.*/}
+				<DisplayGraph data={data}/>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- client/src/components/CurrencyInput.js
+++ client/src/components/CurrencyInput.js
@@ -1,44 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types'
 import './CurrencyInput.css'
 
-export default class CurrencyInput extends Component {
-	constructor(props) {
-		super(props)
+const CurrencyInput = ({ defaultValue, update }) => {
+	const [has_focus, setFocus] = useState(false);
+	const [value, setValue] = useState(defaultValue);
 
-		this.state = {
-			hasFocus: false,
-			value: props.defaultValue
-		}
-	}
-
-	handleChange(e) {
-		const value = e.target.value
-		this.setState({value})
-	}
-
-	handleFocus(e) {
-		this.setState({
-			hasFocus: true
-		})
-	}
-
-	render() {
-		const { defaultValue } = this.props
-		const { value } = this.state
-
-		return (
-			<div className={`currency-input ${defaultValue !== undefined ? 'default-value' : ''}`}>
-				<span>£</span>
-				<input type="text"
-					value={value}
-					onChange={this.handleChange.bind(this)}
-					onFocus={this.handleFocus.bind(this)}/>
-			</div>
-		)
-	}
+	useEffect(() => {
+		update(value);
+	}, [value]);
+	
+	const handleChange = (e) => setValue(e.target.value);
+	const handleFocus = (e) => setFocus(true);
+	
+	return (
+		<div className={`currency-input ${defaultValue !== undefined ? 'default-value' : ''}`}>
+			<span>£</span>
+			<input type="number"
+				value={value}
+				onChange={handleChange}
+				onFocus={handleFocus}/>
+		</div>
+	)
 }
 
 CurrencyInput.propTypes = {
 	defaultValue: PropTypes.number
 }
+
+export default CurrencyInput;
