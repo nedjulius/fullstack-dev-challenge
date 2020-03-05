74fc01236bf030c4309926b3d52c95e732e16d55
nedjulius <juliusned@Juliuss-MBP.lan>
2020-03-04 23:14:55 -0500

    added CORS allowance

--- server/server.js
+++ server/server.js
@@ -8,6 +8,12 @@ app.set('port', (process.env.PORT || 3001));
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+} else {
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 }
 
 app.use(routes);
